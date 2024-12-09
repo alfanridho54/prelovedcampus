e3c701c2b0e7582daf205ea63a1444ccda6763bb
alfanridho <alfanridho507.com>
2024-12-10 06:02:09 +0700

    fix database dan auth

--- dbpreloved.sql
+++ dbpreloved.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 27 Nov 2024 pada 13.41
+-- Waktu pembuatan: 10 Des 2024 pada 00.01
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -115,7 +115,8 @@ INSERT INTO `kategori_produk` (`id`, `kategori`) VALUES
 (1, 'Elektronik'),
 (2, 'Pakaian'),
 (3, 'Perabotan'),
-(4, 'Buku');
+(4, 'Buku'),
+(6, 'tees');
 
 -- --------------------------------------------------------
 
@@ -177,7 +178,30 @@ CREATE TABLE `personal_access_tokens` (
 
 INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `expires_at`, `created_at`, `updated_at`) VALUES
 (1, 'App\\Models\\User', 5, 'token', '0cec8832489a0b9164f5374f42b14deaf83d06e5b261aecf95daf3da21cecbe3', '[\"*\"]', NULL, NULL, '2024-11-27 02:01:20', '2024-11-27 02:01:20'),
-(2, 'App\\Models\\User', 5, 'token', 'f77d2c804e762a4bde8f0548c342dbd4aed2115629958b95f7d035c59264c8da', '[\"*\"]', NULL, NULL, '2024-11-27 02:02:09', '2024-11-27 02:02:09');
+(2, 'App\\Models\\User', 5, 'token', 'f77d2c804e762a4bde8f0548c342dbd4aed2115629958b95f7d035c59264c8da', '[\"*\"]', NULL, NULL, '2024-11-27 02:02:09', '2024-11-27 02:02:09'),
+(3, 'App\\Models\\User', 5, 'token', '287d08309bb151f1d7367bbe9705d5e04003b43c51b436416d103dc2badfedad', '[\"*\"]', NULL, NULL, '2024-12-02 02:05:49', '2024-12-02 02:05:49'),
+(4, 'App\\Models\\User', 5, 'token', 'f80ff0ef67e5788f5d376e867789f82a55508406f495599a31b14f59b8eccc15', '[\"*\"]', '2024-12-02 02:20:07', NULL, '2024-12-02 02:12:03', '2024-12-02 02:20:07'),
+(5, 'App\\Models\\User', 5, 'token', '2462ad43c3e25c82bf3b2bac80a1668f41d07d5597b14ae0a4b9fe7458c3db13', '[\"*\"]', NULL, NULL, '2024-12-02 02:21:15', '2024-12-02 02:21:15'),
+(6, 'App\\Models\\User', 5, 'token', '63a611f908cce4a4e7367096cce454f7f4479388877b1dce6a907f8640db36fa', '[\"*\"]', '2024-12-02 02:31:47', NULL, '2024-12-02 02:21:19', '2024-12-02 02:31:47'),
+(7, 'App\\Models\\User', 5, 'token', '662aff08aa19b9668aac1164b13b2ddd654159315178f1e74607e3d2a166eedc', '[\"*\"]', '2024-12-02 02:40:19', NULL, '2024-12-02 02:31:58', '2024-12-02 02:40:19'),
+(8, 'App\\Models\\User', 5, 'token', '5e1261d554f01711a017406c5f8f1c9c60f320f6bee0ec7cf9d86154fd50bbf7', '[\"*\"]', '2024-12-02 02:52:55', NULL, '2024-12-02 02:40:25', '2024-12-02 02:52:55'),
+(9, 'App\\Models\\User', 5, 'token', 'fb61c023ce7944fbbcbf042487fd9fb9177892cffc6f9659bdd2afd46921a656', '[\"*\"]', NULL, NULL, '2024-12-02 02:42:58', '2024-12-02 02:42:58'),
+(10, 'App\\Models\\User', 5, 'token', '9e222f32a194d9dd5675c2b0555e54845e674157b4e99e063bda9b60b2785fad', '[\"*\"]', '2024-12-02 02:55:23', NULL, '2024-12-02 02:50:59', '2024-12-02 02:55:23'),
+(11, 'App\\Models\\User', 5, 'token', 'a3389f540795423c2841f91bcb0d07480ed353a8ce7e188c1285b13a07310f51', '[\"*\"]', '2024-12-02 16:28:53', NULL, '2024-12-02 16:23:29', '2024-12-02 16:28:53'),
+(12, 'App\\Models\\User', 5, 'token', '1388e5bbcc928cfd9129edcdef47bb821a32247ad270f994bb2a4a4ab5a3891b', '[\"*\"]', '2024-12-02 16:32:37', NULL, '2024-12-02 16:31:46', '2024-12-02 16:32:37'),
+(13, 'App\\Models\\User', 5, 'token', '5fc06f32dedfffe8cfba46d491419c92b81ef18255e99f1868cc3a55666b456e', '[\"*\"]', '2024-12-02 16:35:29', NULL, '2024-12-02 16:32:43', '2024-12-02 16:35:29'),
+(14, 'App\\Models\\User', 5, 'token', '3ae1d9f4ab2ce2897f49732048599b3a7caad3bd752256fcbc70d40822a917d3', '[\"*\"]', '2024-12-02 16:36:48', NULL, '2024-12-02 16:35:33', '2024-12-02 16:36:48'),
+(15, 'App\\Models\\User', 7, 'token', '6bac17cfac5b5be42e0d18287ca3e202d8a48d357aa80ad6319df23535bf0fee', '[\"*\"]', '2024-12-02 16:41:06', NULL, '2024-12-02 16:39:32', '2024-12-02 16:41:06'),
+(16, 'App\\Models\\User', 7, 'token', '5121923b7e849030d06f115efd4da91de9d6dccfc993df38c9c3fec27d10ee92', '[\"*\"]', '2024-12-02 16:48:45', NULL, '2024-12-02 16:41:11', '2024-12-02 16:48:45'),
+(17, 'App\\Models\\User', 8, 'token', 'd8a79d148a32eaf4ba3c8023782bc2d70fff4b1813c742cb969dfd9a573b5c1c', '[\"*\"]', '2024-12-02 17:13:06', NULL, '2024-12-02 17:12:10', '2024-12-02 17:13:06'),
+(18, 'App\\Models\\User', 8, 'token', '5bd78ee7035d275bff6beaf3818ff3283e9cc9e8360a4b0489c07610219dbdb2', '[\"*\"]', NULL, NULL, '2024-12-02 17:15:32', '2024-12-02 17:15:32'),
+(19, 'App\\Models\\User', 10, 'token', '3757100b9eeba833bc57ab0f2d10af89047368ee33642de11515f4ca37f68e15', '[\"*\"]', NULL, NULL, '2024-12-02 17:19:09', '2024-12-02 17:19:09'),
+(20, 'App\\Models\\User', 10, 'token', '3ae2ee7c190f410a5d2d1d14a0720c20d91a39acb78de6fd43d2b57f07d2b333', '[\"*\"]', NULL, NULL, '2024-12-02 17:19:46', '2024-12-02 17:19:46'),
+(21, 'App\\Models\\User', 10, 'token', '72356ef417c355b0dbf9205188581f6e8ce8556a02c84408eb91391113732b2f', '[\"*\"]', '2024-12-03 19:59:53', NULL, '2024-12-03 19:59:43', '2024-12-03 19:59:53'),
+(22, 'App\\Models\\User', 10, 'token', '61140aa420ab8dd8e1d3776c3a1321d6b10ba10a3d6ce1cc189b12c45dafb246', '[\"*\"]', '2024-12-03 20:12:24', NULL, '2024-12-03 20:05:54', '2024-12-03 20:12:24'),
+(23, 'App\\Models\\User', 10, 'token', 'b5f33eec43e5f1b25b364a6e928f1a2499695ba8777e75273a07cb30980de4e4', '[\"*\"]', '2024-12-03 20:34:10', NULL, '2024-12-03 20:15:40', '2024-12-03 20:34:10'),
+(24, 'App\\Models\\User', 10, 'token', 'b32e9012249327c99cc2359ea0cfca0b7b4439914af97a09ac4bf62a1236c2b1', '[\"*\"]', '2024-12-03 20:38:13', NULL, '2024-12-03 20:32:42', '2024-12-03 20:38:13'),
+(25, 'App\\Models\\User', 11, 'token', '2b2b5b66d0bf960c60007a9d1b746d2cea97b1589ffc3b342c0ce28e445634ec', '[\"*\"]', '2024-12-03 20:46:38', NULL, '2024-12-03 20:46:08', '2024-12-03 20:46:38');
 
 -- --------------------------------------------------------
 
@@ -203,29 +227,7 @@ INSERT INTO `produk` (`id`, `nama`, `harga`, `deskripsi`, `stok`, `lokasi_gambar
 (1, 'Laptop ASUS', 7500000.00, 'Laptop ASUS dengan spesifikasi tinggi', 10, 'path/to/laptop_asus.jpg', 1),
 (2, 'Kemeja Pria', 150000.00, 'Kemeja pria bahan katun', 25, 'path/to/kemeja_pria.jpg', 2),
 (3, 'Sofa Minimalis', 2000000.00, 'Sofa minimalis untuk ruang tamu', 5, 'path/to/sofa.jpg', 3),
-(4, 'Buku Pemrograman', 100000.00, 'Buku panduan pemrograman untuk pemula', 30, 'path/to/buku_pemrograman.jpg', 4),
-(5, 'Laptop Lenovo LOQ', 7500000.00, 'kondisi mulus minus pemakaian', 1, '/path/to/loq.jpg', 1);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `role`
---
-
-CREATE TABLE `role` (
-  `id` int(11) NOT NULL,
-  `role` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Dumping data untuk tabel `role`
---
-
-INSERT INTO `role` (`id`, `role`) VALUES
-(1, 'Admin'),
-(2, 'Customer'),
-(3, 'Seller'),
-(4, 'tes add role');
+(4, 'Buku Pemrograman', 100000.00, 'Buku panduan pemrograman untuk pemula', 30, 'path/to/buku_pemrograman.jpg', 4);
 
 -- --------------------------------------------------------
 
@@ -247,7 +249,9 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('aKy2Zwiflepx3pSOkldaQIu1fdrkFRErMrOXUvDu', NULL, '127.0.0.1', 'PostmanRuntime/7.43.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiNVVYZTdQemVUWnFpa1NHWHh5S0xXWWd3aVhZVnpPR1BJdVFqdnBzTiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1733133323),
 ('g0n3CXerg6lzVUC8kClNYFqKZraJMZaTjMtDI2qs', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoibEFHWm9MWHBLREtkUjJXUVBVdmFmZDJsZUtDaHNhcm9qUmVybGV5dSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1731062362),
+('g47lO861ChMFtZcvGgBeBGHbW83V4drhVkalL14Y', NULL, '127.0.0.1', 'PostmanRuntime/7.43.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoicG15MGpoMk9jSk1hbnNEeEhydDhlbU9POUJKTlZIbjcwV2s1MklkbCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1733182133),
 ('kxkEkkpOnlxnmHULQOgkfZcm8MpBuuM2qBNkVgWR', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiVWIwZlN6aVFPd3ZSc0NuZGF0WVpWR2JRWlFhNUhBMUNINmlXTDRsZCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1731330019);
 
 -- --------------------------------------------------------
@@ -287,17 +291,6 @@ CREATE TABLE `transaksi` (
   `status_pembayaran_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
---
--- Dumping data untuk tabel `transaksi`
---
-
-INSERT INTO `transaksi` (`id`, `jumlah_beli`, `total_harga`, `metode_pembayaran`, `tanggal_transaksi`, `user_id`, `produk_id`, `status_pembayaran_id`) VALUES
-(1, 1, 7500000.00, 'Transfer Bank', '2024-11-08 08:30:00', 1, 1, 1),
-(2, 2, 300000.00, 'Kartu Kredit', '2024-11-08 09:00:00', 2, 2, 2),
-(3, 1, 2000000.00, 'Cash', '2024-11-08 10:00:00', 3, 3, 1),
-(4, 3, 300000.00, 'E-Wallet', '2024-11-08 11:30:00', 1, 4, 3),
-(5, 1, 7500000.00, 'Kartu Kredit', '2024-11-08 08:30:00', 1, 5, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -312,37 +305,18 @@ CREATE TABLE `user` (
   `foto` longtext DEFAULT NULL,
   `no_hp` int(11) DEFAULT NULL,
   `alamat` longtext DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL
+  `role` enum('admin','customer','penjual','guest') NOT NULL DEFAULT 'guest'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id`, `nama`, `email`, `password`, `foto`, `no_hp`, `alamat`, `role_id`) VALUES
-(1, 'John Doe', 'johndoe@example.com', 'password123', 'path/to/johndoe.jpg', 2147483647, 'Jl. Kebon Jeruk No. 10', 1),
-(2, 'Jane Smith', 'janesmith@example.com', 'password456', 'path/to/janesmith.jpg', 2147483647, 'Jl. Mangga No. 5', 2),
-(3, 'Robert Brown', 'robertbrown@example.com', 'password789', 'path/to/robertbrown.jpg', 2147483647, 'Jl. Durian No. 12', 3),
-(4, 'budi', 'budi@gmail', '$2y$12$MOcIlpWgEdBDwmzbE9g5Xu43BALUTfmvTL7ANca2oKQiTZ8eTVV8.', NULL, 9088877, 'gg boan', 3),
-(5, 'pengguna1', 'abcde@gmail.com', '$2y$12$R6zsB2iyC2CtkqPlM7EhAe98kqAm6U9Q4nscbHrQHpd.bU3ilypCC', NULL, NULL, NULL, NULL),
-(6, 'Muhammad Alfan Ridho', 'alfan@example.com', '$2y$12$B2/XI0CCVx5YnOJ/vNLY4O.dXP8k2hPbXsFBx934jiiXAp/vpk55y', 'path/to/alfan.jpg', 81245679, 'Depok, Indonesia', 2);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `users`
---
-
-CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) NOT NULL,
-  `remember_token` varchar(100) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `user` (`id`, `nama`, `email`, `password`, `foto`, `no_hp`, `alamat`, `role`) VALUES
+(8, 'admin', 'admin@gmail.com', '$2y$12$S/5GaYh6uWZlQqO6LglMo.MnnexIlhI2Pz7AKKUZ.cnDj95iGCa1K', NULL, NULL, NULL, 'guest'),
+(9, 'admin', 'admin@gmail.com', '$2y$12$qcxxvxEcWc2sTgWAHLX1huyWLn4kcOA8949qG80CFfIuuSOr6CgTO', NULL, NULL, NULL, 'admin'),
+(10, 'admin', 'admin1@gmail.com', '$2y$12$eG6aPoAb9w2ONTeYgoyQRuEIfnR4zJIcUTGscfCp/MMwi5LoPMJum', NULL, NULL, NULL, 'admin'),
+(11, 'customer', 'customer@gmail.com', '$2y$12$QeTWsvyh/9XywGYDckTyPuj.heeVKV/SAE6Dt2ABcBTGvmNQiTW1e', NULL, NULL, NULL, 'customer');
 
 --
 -- Indexes for dumped tables
@@ -414,12 +388,6 @@ ALTER TABLE `produk`
   ADD KEY `kategori_produk_id` (`kategori_produk_id`);
 
 --
--- Indeks untuk tabel `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indeks untuk tabel `sessions`
 --
 ALTER TABLE `sessions`
@@ -446,15 +414,7 @@ ALTER TABLE `transaksi`
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `role_id` (`role_id`);
-
---
--- Indeks untuk tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -476,7 +436,7 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT untuk tabel `kategori_produk`
 --
 ALTER TABLE `kategori_produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `migrations`
@@ -488,19 +448,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT untuk tabel `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT untuk tabel `produk`
 --
 ALTER TABLE `produk`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT untuk tabel `role`
---
-ALTER TABLE `role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `status_pembayaran`
@@ -518,13 +472,7 @@ ALTER TABLE `transaksi`
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT untuk tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -543,12 +491,6 @@ ALTER TABLE `transaksi`
   ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`produk_id`) REFERENCES `produk` (`id`),
   ADD CONSTRAINT `transaksi_ibfk_3` FOREIGN KEY (`status_pembayaran_id`) REFERENCES `status_pembayaran` (`id`);
-
---
--- Ketidakleluasaan untuk tabel `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
